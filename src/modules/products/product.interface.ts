bab2b2af6c8e6eafa85b175283b92f3dafe90a71
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-21 16:45:26 +0600

    product interface created

---
+++ src/modules/products/product.interface.ts
@@ -0,0 +1,11 @@
+export type TProduct = {
+    name: string;
+    description: string;
+    price: number;
+    stockQuantity: number;
+    category: string;
+    images: string[];
+    slug: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
