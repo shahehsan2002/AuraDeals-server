93b9c9387045729a0d3d53cec8c7c9e86cb2d2b8
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-21 17:00:57 +0600

    products model created

---
+++ src/modules/products/product.model.tsx
@@ -0,0 +1,48 @@
+import { model, Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stockQuantity: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  updatedAt: { 
+    type: Date, 
+    required: true
+ },
+});
+
+export const product = model<TProduct>(
+  "Product",
+  productSchema,
+//   "products",
+//   {}
+);
