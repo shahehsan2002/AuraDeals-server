f0ebe3f935bbefed97bccb1717c95a1c450ac873
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-21 18:01:47 +0600

    get all products service created

--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 // Create product route
 router.post('/',ProductControllers.createProduct);
 
+// Get all products route
+router.get('/',ProductControllers.getAllProducts);
+
 
 export const ProductRoutes = router
--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -3,10 +3,17 @@ import { product } from "./product.model";
 
 // Create a new product
 const createProduct = async (payload: TProduct) => {
-    const result = await product.create(payload);
-    return result;
+  const result = await product.create(payload);
+  return result;
+};
+
+// Get all products
+const getAllProducts = async () => {
+  const result = await product.find();
+  return result;
 };
 
 export const ProductServices = {
-    createProduct
-}
+  createProduct,
+  getAllProducts,
+};
